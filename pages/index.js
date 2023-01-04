a43a8334d4b2df4793913f96e01e5eeec2f8fb3e
THREEhundo <samuelbaik87@gmail.com>
2023-01-04 14:29:09 -0800

    added head to about & index

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,11 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
 	return (
 		<div>
+			<Head>
+				<title>WebDev Newz</title>
+				<meta name='keywords' content='web development, progrmming' />
+			</Head>
+
 			<h1>Welcome to Next</h1>
 		</div>
 	)

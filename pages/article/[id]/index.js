a4472a5334cef368f0f4d22bbe7313cac30fe2e3
THREEhundo <samuelbaik87@gmail.com>
2023-01-04 16:44:31 -0800

    getStaticPaths()

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -7,7 +7,7 @@ const article = ({ article }) => {
 
 	return (
 		<>
-			<h1>>this is an article {article.id}</h1>
+			<h1>this is article {article.id}</h1>
 			<p>{article.body}</p>
 			<br />
 			<Link href='/'>Go Back</Link>
@@ -15,7 +15,20 @@ const article = ({ article }) => {
 	)
 }
 
-export const getServerSideProps = async context => {
+//export const getServerSideProps = async context => {
+//	const res = await fetch(
+//		`https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//	)
+//
+//	const article = await res.json()
+//
+//	return {
+//		props: {
+//			article
+//		}
+//	}
+//}
+export const getStaticProps = async context => {
 	const res = await fetch(
 		`https://jsonplaceholder.typicode.com/posts/${context.params.id}`
 	)
@@ -26,6 +39,20 @@ export const getServerSideProps = async context => {
 		props: {
 			article
 		}
+	}
+}
+
+export const getStaticPaths = async () => {
+	const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+	const articles = await res.json()
+
+	const ids = articles.map(article => article.id)
+
+	const paths = ids.map(id => ({ params: { id: id.toString() } }))
+	return {
+		paths,
+		fallback: false
 	}
 }
 export default article

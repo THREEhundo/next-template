597e59a932c7cf789bd670429d737ad8d6a89e1a
THREEhundo <samuelbaik87@gmail.com>
2023-01-04 16:35:59 -0800

    getServerSideProps()

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,11 +1,31 @@
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-const article = () => {
+const article = ({ article }) => {
 	//const router = useRouter()
 	//const { id } = router.query
 
-	return <div>this is an article {id} </div>
+	return (
+		<>
+			<h1>>this is an article {article.id}</h1>
+			<p>{article.body}</p>
+			<br />
+			<Link href='/'>Go Back</Link>
+		</>
+	)
 }
 
-export const getServerSideProps
+export const getServerSideProps = async context => {
+	const res = await fetch(
+		`https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+	)
+
+	const article = await res.json()
+
+	return {
+		props: {
+			article
+		}
+	}
+}
 export default article

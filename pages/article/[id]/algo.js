0253c4326d1d9e511af850873c7eb788378cae48
THREEhundo <samuelbaik87@gmail.com>
2023-01-09 20:18:26 -0800

    algo

---
+++ pages/article/[id]/algo.js
@@ -0,0 +1,27 @@
+//*** Sunrise Algo */
+
+//* Variables
+//* Waking time
+//* Temperature Minimum - (2 Hours Before Average Waking Time)
+//* Bedtime
+//* sunlight viewing time
+//*
+//*
+//* Increase in core body temperature due to cortisol pulse
+//* Viewing light ensures that the cortisol pulse happens at the same time the next day.
+//* Your body clocks are matched to this cortisol pulse.
+//* Retinal Ganglion Cells ==> Hypothalamus ==> peptide (signal)  ^ Temp Rise to Max
+
+//* Output --> Array of objects?
+
+//*** Going East vs. West  ***/
+
+//* Travel Fatigue & Timezone Jetlag --> Inability of local sunlight & local darkness to match your internal rythmn
+//*
+
+//*** Temperature Minimum ***/
+
+//* Phase Advance - Viewing light 4-5 hours after your temperature minimum will shift so that you will wake up earlier and go to bed earlier.
+//* Phase Delay - Viewing bright light before your temperature minimum will make you go to bed later and wake up later.
+//*
+//*
